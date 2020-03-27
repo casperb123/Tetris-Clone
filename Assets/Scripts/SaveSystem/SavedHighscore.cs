a2bbee7756542565113e790e7f4663f624d4c25f
casperb123 <42714453+casperb123@users.noreply.github.com>
2020-03-27 11:07:54 +0100

    Fixed highscore date not being set

--- Assets/Scripts/SaveSystem/SavedHighscore.cs
+++ Assets/Scripts/SaveSystem/SavedHighscore.cs
@@ -5,4 +5,9 @@ public class SavedHighscore
 {
     public int Score { get; set; }
     public DateTime? Date { get; set; }
+
+    public SavedHighscore()
+    {
+        Date = DateTime.Now;
+    }
 }
